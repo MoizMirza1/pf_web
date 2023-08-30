88cb610b4bd986ec2bd800a1e6c2bd040c50ca88
Moiz <moizmirza212@gmail.com>
2023-08-30 17:00:20 +0500

    version1

---
+++ src/Components/Header.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import logo from '../assets/img/My logo.png'
+import Nav from './Nav.js'
+import NavMobile from './NavMobile.js'
+import Socials from '../Components/Socials.js'
+
+
+const Header = () => {
+
+    const [bg, setBg] = useState(false);
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 50){
+                setBg(true);
+            } else {
+                setBg(false);
+            }
+            
+        })
+    }, [])
+
+    const logoSize = bg ? "h-12" : "h-14";
+
+  return (
+    <header className={`${bg ? "bg-dark-900 h-20" : "h-24 bg-black"} flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300 `}>
+        <div className='container mx-auto flex items-center justify-between h-full '>
+
+          {/* nav logo */}
+        <a href="#">
+          <img src={logo} alt="" className={`mr-2 ${logoSize}`}/>
+        </a>
+
+        {/* nav  */} 
+
+        <div className='hidden lg:block'>
+          <Nav/>
+        </div>
+        
+        {/* socials */}
+
+        <div className='hidden lg:block'>
+          <Socials/>
+        </div>
+        
+        {/* nav mobile */}
+        <div className='block lg:hidden'>
+            <NavMobile/>
+          </div>
+
+
+        </div>
+    </header>
+
+      
+  )
+}
+
+export default Header
---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// import { navigation } from '../data'
+
+
+// React Router
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul className='flex space-x-8 capitalize text-[15px]'>
+
+
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
